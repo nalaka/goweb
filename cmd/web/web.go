2ec35a6db30e6f3c749fc8fd333fc5a48ce25bd2
Nalaka Jayasena <437578+nalaka@users.noreply.github.com>
2024-02-18 02:29:53 -0800

    Web command using internal books package

---
+++ cmd/web/web.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nalaka/goweb/internal/books"
+)
+
+func main() {
+	fmt.Println(books.BOOKS)
+}
---
+++ internal/books/books.go
@@ -0,0 +1,7 @@
+package books
+
+var BOOKS = []string{
+	"Rendezvous with Rama",
+	"The Caves of Steel",
+	"The Complete Robot",
+}
