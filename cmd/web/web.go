8eb9c74b43beccd23cfef904eeba673fd552c1ab
Nalaka Jayasena <437578+nalaka@users.noreply.github.com>
2024-02-29 16:04:23 -0800

    Serve text response for GET /books

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/nalaka/goweb/internal/books"
 )
 
+const PORT int = 8042
+
 func main() {
-	fmt.Println(books.BOOKS)
+	http.HandleFunc("/books", booksHandler)
+	fmt.Printf("goweb: listening at port %d\n", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%d", PORT), nil))
+}
+
+func booksHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%v\n", books.BOOKS)
 }
